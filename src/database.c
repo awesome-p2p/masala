b8e179ae64057bd1f926ab8cd2242a02927894f2
Moritz Warning <moritzwarning@web.de>
2013-05-20 12:00:47 +0200

    fix project name

--- src/database.c
+++ src/database.c
@@ -1,14 +1,14 @@
 /*
 Copyright 2011 Aiko Barz
 
-This file is part of Torrentkino.
+This file is part of masala.
 
-Torrentkino is free software: you can redistribute it and/or modify
+masala is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Torrentkino is distributed in the hope that it will be useful,
+masala is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
